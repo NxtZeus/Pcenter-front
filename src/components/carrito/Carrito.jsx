348bf4ea52ae7b04c8254e9bd9dc4de02e19c003
NxtZeus <nxtzeus4ever@gmail.com>
2024-06-13 16:57:46 +0200

    arreglo imagenes carrito

--- src/components/carrito/Carrito.jsx
+++ src/components/carrito/Carrito.jsx
@@ -21,7 +21,7 @@ const Carrito = ({ onCerrar, onEliminarItem, onIncrementarItem, onDecrementarIte
 
     // Obtener la URL de la imagen del producto o una imagen por defecto si no hay URL o es null o undefined
     const obtenerUrlImagen = (url) => {
-        const URLbase = 'https://tfg-backend-production-5a6a.up.railway.app';
+        const URLbase = 'http://tfg-backend-production-5a6a.up.railway.app';
         if (!url) {
             return `${URLbase}/default-image.jpg`; // Ruta de una imagen por defecto si url es undefined o null
         }
