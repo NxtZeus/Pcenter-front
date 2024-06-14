80f9b14b8d926fca5a322736689e666e169a91c9
NxtZeus <nxtzeus4ever@gmail.com>
2024-06-14 20:50:51 +0200

    fix menor

--- src/pages/InfoProductos.jsx
+++ src/pages/InfoProductos.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { obtenerProductos } from '../components/apis/Api';
 import Carrousel from '../components/carrousel/Carrousel';
